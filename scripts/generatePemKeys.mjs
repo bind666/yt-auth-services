1bcde460a277d71681c2890e079b08953952529d
bind666 <sachinbind9891@gmail.com>
2024-09-23 18:16:02 +0530

    add

--- scripts/generatePemKeys.mjs
+++ scripts/generatePemKeys.mjs
@@ -1,24 +1,24 @@
-import { generateKeyPairSync } from "crypto";
-import fs from 'fs'
+import { generateKeyPairSync } from 'crypto';
+import fs from 'fs';
 
 // Function to generate keys
 function generateKeyPair() {
-    const {privateKey, publicKey} = generateKeyPairSync('rsa',{
-        modulusLength: 2048, // The length of the key in bits
-        publicKeyEncoding:{
-            type:'pkcs1',
-            format:'pem',
-        },
-        privateKeyEncoding:{
-            type:'pkcs1',
-            format:'pem',
-        },
-    });
+  const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+    modulusLength: 2048, // The length of the key in bits
+    publicKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem',
+    },
+    privateKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem',
+    },
+  });
 
-    fs.writeFileSync('../certs/private_key.pem', privateKey);
-    fs.writeFileSync('../certs/public_key.pem', publicKey);
+  fs.writeFileSync('../certs/private_key.pem', privateKey);
+  fs.writeFileSync('../certs/public_key.pem', publicKey);
 
-    return {privateKey, publicKey};
+  return { privateKey, publicKey };
 }
 
-generateKeyPair()
+generateKeyPair();
