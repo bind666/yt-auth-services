47c2c22e7e7b1ed7c65aa80c9d7e246308b5d4cc
bind666 <sachinbind9891@gmail.com>
2024-09-25 16:31:17 +0530

    add rsaToJwk

--- scripts/rsaToJwk.mjs
+++ scripts/rsaToJwk.mjs
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import rsaPemToJwk from 'rsa-pem-to-jwk';
 
-
 var pem = fs.readFileSync('./certs/private_key.pem');
 var jwk = rsaPemToJwk(pem, { use: 'sig' }, 'public');
 
